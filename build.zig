aea2c9b9bee8277bea761009416996104510e130
Sam Connelly <myclevorname@gmail.com>
2025-05-22 20:49:16 -0400

    stuff

--- build.zig
+++ build.zig
@@ -4,19 +4,25 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib_mod = b.addModule("narser", .{
+    const narser = b.addModule("narser", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    const exe_mod = b.addModule("narser_bin", .{
+    const exe_mod = b.addModule("narse", .{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    exe_mod.addImport("narser", lib_mod);
+    exe_mod.addImport("narser", narser);
+
+    const lib_mod = b.addModule("libnarser", .{
+        .root_source_file = b.path("src/lib.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
 
     const lib = b.addLibrary(.{
         .linkage = .static,
@@ -44,11 +50,11 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const lib_unit_tests = b.addTest(.{
-        .root_module = lib_mod,
+    const narser_unit_tests = b.addTest(.{
+        .root_module = narser,
     });
 
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+    const run_narser_unit_tests = b.addRunArtifact(narser_unit_tests);
 
     const exe_unit_tests = b.addTest(.{
         .root_module = exe_mod,
@@ -57,6 +63,6 @@ pub fn build(b: *std.Build) void {
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
     test_step.dependOn(&run_exe_unit_tests.step);
+    test_step.dependOn(&run_narser_unit_tests.step);
 }
