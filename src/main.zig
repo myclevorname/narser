429409bf76a16142b459823c0d6f304ac4c567fe
Sam Connelly <myclevorname@gmail.com>
2025-06-02 07:33:17 -0400

    Add cat command

--- src/main.zig
+++ src/main.zig
@@ -70,13 +70,13 @@ pub fn main() !void {
     const command = args.next() orelse fatal("No command supplied", .{});
 
     if (std.mem.eql(u8, "pack", command)) {
-        var argument = args.next();
-        if (argument == null or std.mem.eql(u8, "-", argument.?)) argument = "/dev/fd/0";
-        const stat = try std.fs.cwd().statFile(argument.?);
+        var argument = args.next() orelse "-";
+        if (std.mem.eql(u8, "-", argument)) argument = "/dev/fd/0";
+        const stat = try std.fs.cwd().statFile(argument);
         switch (stat.kind) {
             .sym_link => {
                 var buffer: [std.fs.max_path_bytes]u8 = undefined;
-                const target = try std.fs.cwd().readLink(argument.?, &buffer);
+                const target = try std.fs.cwd().readLink(argument, &buffer);
 
                 var archive = try narser.NarArchive.fromSymlink(allocator, target);
                 defer archive.deinit();
@@ -84,7 +84,7 @@ pub fn main() !void {
                 try archive.dump(writer);
             },
             .directory => {
-                var dir = try std.fs.cwd().openDir(argument.?, .{ .iterate = true });
+                var dir = try std.fs.cwd().openDir(argument, .{ .iterate = true });
                 defer dir.close();
                 var archive = try narser.NarArchive.fromDirectory(allocator, dir);
                 defer archive.deinit();
@@ -92,7 +92,7 @@ pub fn main() !void {
                 try archive.dump(writer);
             },
             else => {
-                const contents = try std.fs.cwd().readFileAlloc(allocator, argument.?, std.math.maxInt(usize));
+                const contents = try std.fs.cwd().readFileAlloc(allocator, argument, std.math.maxInt(usize));
                 defer allocator.free(contents);
 
                 var archive = try narser.NarArchive.fromFileContents(
@@ -116,5 +116,67 @@ pub fn main() !void {
         var archive = try narser.NarArchive.fromSlice(allocator, contents);
         defer archive.deinit();
         try lsRecursive(&archive, writer);
+    } else if (std.mem.eql(u8, "cat", command)) {
+        var archive_path = args.next() orelse "-";
+        if (std.mem.eql(u8, "-", archive_path)) archive_path = "/dev/fd/0";
+        const subpath = args.next() orelse ".";
+
+        const contents = try std.fs.cwd().readFileAlloc(allocator, archive_path, std.math.maxInt(usize));
+        defer allocator.free(contents);
+
+        var archive = try narser.NarArchive.fromSlice(allocator, contents);
+        defer archive.deinit();
+
+        var cur = archive.root;
+
+        switch (cur.data) {
+            .directory => |child| if (child == null) fatal("Archive \"{s}\" is an empty directory", .{archive_path}),
+            .file => {},
+            .symlink => fatal("narser does not support following symbolic links to the filesystem", .{}),
+        }
+
+        var symlinks: std.BoundedArray([]const u8, 4096) = .{};
+
+        symlinks.appendAssumeCapacity(subpath);
+
+        comptime std.debug.assert(std.fs.path.sep == '/');
+        while (symlinks.pop()) |full_first_path| {
+            const first_part_len = std.mem.indexOfScalar(u8, full_first_path, '/');
+            const first = if (first_part_len) |len| full_first_path[0..len] else full_first_path;
+            const rest = if (first_part_len) |len| full_first_path[len+1..] else "";
+            if (rest.len != 0) symlinks.appendAssumeCapacity(rest);
+
+            if (std.mem.eql(u8, first, ".") or first.len == 0) continue;
+            if (first.len == 0) fatal("narser does not support following symbolic links to the filesystem", .{});
+            if (std.mem.eql(u8, first, "..")) {
+                if (cur.parent.?.name) |_| cur = cur.parent.?;
+            }
+
+            cur = if (cur.data == .directory)
+                cur.data.directory orelse return error.FileNotFound
+            else @panic(cur.name.?);
+            find: while (true) {
+                switch (std.mem.order(u8, cur.name.?, first)) {
+                    .lt => {},
+                    .eq => break :find,
+                    .gt => return error.FileNotFound,
+                }
+                cur = cur.next orelse return error.FileNotFound;
+            }
+            if (symlinks.len != 0) switch (cur.data) {
+                .directory => {},
+                .file => return error.IsFile,
+                .symlink => |target| {
+                    try symlinks.append(target);
+                    cur = cur.parent.?;
+                },
+            };
+        }
+
+        switch (cur.data) {
+            .file => |metadata| try writer.writeAll(metadata.contents),
+            .symlink => unreachable,
+            .directory => fatal("Expected file, found directory", .{}),
+        }
     } else fatal("Invalid command '{s}'", .{command});
 }
